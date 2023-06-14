378ea6f0c3c47445e8bd13ec07989dbee5ea05a7
Md. Ali Azgor <aliazgoryeasin@gmail.com>
2023-06-14 13:48:43 +0600

    verify jwt works apis done

--- index.js
+++ index.js
@@ -4,11 +4,30 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-var jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@fluent-friends-cluster.yutyvxi.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -37,6 +56,15 @@ async function run() {
       .db("Fluent-Friends-DB")
       .collection("selectedClasses");
 
+    // jwt
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send(token);
+    });
+
     // Basic Apis
     app.get("/classes", async (req, res) => {
       const result = await classesCollection.find().toArray();
@@ -55,11 +83,19 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/selectedClasses", async (req, res) => {
+    app.get("/selectedClasses", verifyJWT, async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([]);
       }
+
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res
+        .status(403)
+        .send({ error: true, message: "forbidden access" });
+      }
+
       const query = { email: email };
       const result = await selectedClassesCollection.find(query).toArray();
       res.send(result);
@@ -73,7 +109,7 @@ async function run() {
     });
 
     // Admin works apis
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyJWT, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
@@ -83,47 +119,58 @@ async function run() {
       const query = { email: user?.email };
       const existingUser = await usersCollection.findOne(query);
       if (existingUser) {
-        res.send({ message: "existing user" });
+       return res.send({ message: "existing user" });
       }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
 
-    app.patch('/users/student/:id', async(req, res) => {
-      const id = req.params.id; 
-      const filter = { _id: new ObjectId(id)}
-      const updateDoc = {
-        $set: {
-          role: "student"
-        }
+    app.get('/users/admin/:email', verifyJWT, async(req, res)=>{
+      const email = req.params.email
+      if(req.decoded.email !== email){
+        return res.send({admin: false})
       }
-      const result = await usersCollection.updateOne(filter, updateDoc)
+      const query = {email: email}
+      const user = await usersCollection.findOne(query)
+      const result = {admin: user?.role === 'admin'}
       res.send(result)
     })
 
-    app.patch('/users/instructor/:id', async(req, res) => {
-      const id = req.params.id; 
-      const filter = { _id: new ObjectId(id)}
+    app.patch("/users/student/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-          role: "instructor"
-        }
-      }
-      const result = await usersCollection.updateOne(filter, updateDoc)
-      res.send(result)
-    })
+          role: "student",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
-    app.patch('/users/admin/:id', async(req, res) => {
-      const id = req.params.id; 
-      const filter = { _id: new ObjectId(id)}
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-          role: "admin"
-        }
-      }
-      const result = await usersCollection.updateOne(filter, updateDoc)
-      res.send(result)
-    })
+          role: "instructor",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
